6e717cf1542199afa63336e93111ea1ece404476
prasenjit52282 <prasenjitkarmakar52282@gmail.com>
2023-10-06 03:00:37 +0530

    removed make from install script

--- bin/installdocker.sh
+++ bin/installdocker.sh
@@ -25,6 +25,3 @@ sudo curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-
-sudo apt-get install make
